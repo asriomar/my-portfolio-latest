a412754d6bf685a14894074765f577a93e5e1aaa
Mohd Asri Omar <62752924+asriomar@users.noreply.github.com>
2024-06-13 13:53:47 +0800

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,9 @@ const Header = () => {
   return (
     <header className="bg-black text-white py-4 shadow-lg">
       <div className="container mx-auto flex flex-col md:flex-row justify-around items-center px-4 md:px-0">
-        <h1 className="text-xl text-gray-200 md:text-3xl font-extrabold cursor-pointer mb-2 md:mb-0 flex text-center bg-clip-text text-transparent bg-gradient-to-r from-sky-500 to-indigo-500">
+        <h1 className="text-xl text-gray-200 md:text-3xl font-extrabold cursor-pointer mb-2 md:mb-0 flex text-center ">
           &#123;
-          <span className="underline underline-offset-4 decoration-red-600 decoration-wavy">
+          <span className="">
             md.dev
           </span>
           &#125;
@@ -38,3 +38,6 @@ const Header = () => {
 };
 
 export default Header;
+
+// bg-clip-text text-transparent bg-gradient-to-r from-sky-500 to-indigo-500
+// underline underline-offset-4 decoration-red-600 decoration-wavy
