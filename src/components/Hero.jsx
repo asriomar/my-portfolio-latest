03ac6e6ce92b56a1414552f83026894f311412da
Mohd Asri Omar <62752924+asriomar@users.noreply.github.com>
2024-06-13 13:53:20 +0800

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,28 +1,25 @@
 // src/components/Hero.js
 import React from 'react';
+import './Hero.css'; // Make sure to create and import a CSS file for additional styles
 
 const Hero = () => {
   return (
-    <section
-      className="flex flex-col items-center justify-center md:h-screen bg-black "
-     
-    >
-      
-      <div className="px-10 py-7 rounded-lg text-center ">
-        <h1 className="text-gray-200 text-3xl md:text-8xl font-bold mb-6">
+    <section className="hero-container flex flex-col items-center justify-center md:h-screen bg-black">
+      <div className="hero-content px-10 py-7 rounded-lg text-center">
+        <h1 className="hero-title text-gray-200 text-3xl md:text-8xl font-bold mb-6">
           I'm{' '}
-          <span className="bg-clip-text text-transparent bg-gradient-to-r from-indigo-800 via-blue-700 to-green-700 underline decoration-wavy decoration-red-600">
+          <span className=" underline decoration-wavy decoration-red-600">
             &#123;md.dev&#125;
           </span>
         </h1>
-        <p className="text-gray-200 text-md md:text-xl mb-8">
+        <p className="hero-subtitle text-gray-200 text-md md:text-xl mb-8">
           Ready to embrace new challenges, bring valuable insights <br />
           and drive for excellence to make a meaningful impact{' '}
-          <span className="md:text-2xl">👋</span>
+          <span className="wave-emoji md:text-2xl">👋</span>
         </p>
         <a
           href="#projects"
-          className="bg-gradient-to-r from-indigo-800 via-purple-800 to-pink-800 hover:bg-blue-700 text-white font-semibold py-3 px-6 rounded-full transition-transform transform hover:scale-105"
+          className="hero-button bg-gradient-to-r from-indigo-800 via-purple-800 to-pink-800 hover:bg-blue-700 text-white font-semibold py-3 px-6 rounded-full transition-transform transform hover:scale-105"
         >
           View My Projects
         </a>
