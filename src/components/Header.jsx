ed52de38454c25aacb567fcc8222bd66284aa4be
Mohd Asri Omar <62752924+asriomar@users.noreply.github.com>
2024-09-09 11:48:22 +0800

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,11 @@
-// src/components/Header.js
 import React from 'react';
 
 const Header = () => {
   return (
     <header className="bg-black text-white py-4 shadow-lg">
       <div className="container mx-auto flex flex-col md:flex-row justify-around items-center px-4 md:px-0">
-        <h1 className=" text-xl md:text-3xl  text-red-500 md:text-3xl font-extrabold cursor-pointer mb-4 md:mb-0 flex text-center ">
-          &#123;
-          
-            mo.asri
-         
-          &#125;
+        <h1 className="text-xl md:text-3xl text-red-500 font-extrabold cursor-pointer mb-4 md:mb-0 flex text-center">
+          {"{ mo.asri }"}
         </h1>
         <nav className="flex gap-6 text-sm md:text-xl">
           <a
@@ -44,6 +39,3 @@ const Header = () => {
 };
 
 export default Header;
-
-// bg-clip-text text-transparent bg-gradient-to-r from-sky-500 to-indigo-500
-// underline underline-offset-4 decoration-red-600 decoration-wavy
