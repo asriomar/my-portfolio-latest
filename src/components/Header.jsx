dbc3e575fdde1f1413ba6a3d27dfb3a34d333bac
Mohd Asri Omar <62752924+asriomar@users.noreply.github.com>
2024-09-10 13:07:31 +0800

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className="bg-black text-white py-4 shadow-lg">
       <div className="container mx-auto flex flex-col md:flex-row justify-around items-center px-4 md:px-0">
-        <h1 className="text-xl md:text-3xl text-indigo-200 font-extrabold cursor-pointer mb-4 md:mb-0 flex text-center">
+        <h1 className="text-2xl md:text-3xl text-indigo-200 font-extrabold cursor-pointer mb-4 md:mb-0 flex text-center">
           mo.asri
         </h1>
         <nav className="flex gap-6 text-sm md:text-xl">
